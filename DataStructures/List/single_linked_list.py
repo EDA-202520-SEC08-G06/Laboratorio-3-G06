c232da7c22028265a53ac1a7e6de343bfc8bf70f
samucass07 <sammygc11@gmail.com>
2025-08-22 16:54:56 -0500

    subo funciones

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -12,7 +12,7 @@ def get_element(my_list, pos):
         searchpos+=1
     return node["info"]
     
-    def is_present(my_list, element, cmp_function):
+def is_present(my_list, element, cmp_function):
         is_in_array=False
         temp=my_list["first"]
         count=0
@@ -26,4 +26,23 @@ def get_element(my_list, pos):
             count=-1
         return count 
     
-    
+from DataStructures.List.list_node import new_single_node 
+
+#add first
+def add_first(my_list, element):
+    newnode=new_single_node(element)
+    newnode["next"]=my_list["first"]
+    my_list["first"]=newnode
+    my_list["size"]+=1
+    return my_list
+
+#add last
+def add_last(my_list, element):
+    newnode=new_single_node(element)
+    if my_list["size"]>0:
+        my_list["last"]["next"]=newnode
+    else:
+        my_list["first"]=newnode
+    my_list["last"]=newnode
+    my_list["size"]+=1
+    return my_list
