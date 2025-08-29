9689d810903fa3eaa7b6f5e3c49999e8c0681e41
NeuralKaizen <neuralkaizen@gmail.com>
2025-08-28 21:34:45 -0500

    array_list.py listo para revision

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -24,7 +24,7 @@ def is_present(my_list, element, cmp_function):
 
 
 
-def add_first(my_list:array_list, element) -> array_list: 
+def add_first(my_list, element): 
 
     my_list["elements"] = [element] + my_list["elements"]
     my_list["size"] = my_list["size"] + 1 
@@ -33,7 +33,7 @@ def add_first(my_list:array_list, element) -> array_list:
 
 
 
-def add_last(my_list:array_list, element) -> array_list:
+def add_last(my_list, element):
 
     my_list["elements"] = my_list["elements"] + [element]
     my_list["size"] = my_list["size"] + 1 
@@ -42,101 +42,105 @@ def add_last(my_list:array_list, element) -> array_list:
 
 
 
-def size(my_list:array_list) -> int: 
+def size(my_list) -> int: 
 
     return my_list["size"]
 
 
 
-def first_element(my_list:array_list): 
+def is_empty(my_list) -> bool:
 
-    return get_element(my_list, 0)
-
-
-
-def is_empty(my_list:array_list) -> bool:
-
-    if my_list["size"] == 0: 
-        return True
-
-    else:     
-        return False
+    return my_list["size"] == 0
 
 
 
-def last_element(my_list:array_list): 
+def first_element(my_list): 
 
-    return get_element(my_list,my_list["size"]-1)
+    if is_empty(my_list): 
+        raise Exception('IndexError: list index out of range')
+    
+    else:
+        return get_element(my_list, 0)
 
 
 
-def delete_element(my_list:array_list, index:int) -> array_list: 
+def last_element(my_list): 
+
+    if is_empty(my_list): 
+        raise Exception('IndexError: list index out of range')
+    
+    else:
+        return get_element(my_list, my_list["size"]-1)
+
+
+
+def delete_element(my_list, index:int): 
 
     
-    if index <= my_list["size"] and index >= 0: 
-        
+    if index >= my_list["size"] or index < 0: 
+        raise Exception('IndexError: list index out of range')
+            
+    else:
         my_list["elements"] = my_list["elements"][:index] + my_list["elements"][index+1:] 
         my_list["size"] = my_list["size"] - 1
         
         return my_list
-    
-    else:
-        return my_list
 
 
 
-def remove_first (my_list:array_list):
+def remove_first (my_list):
 
     if is_empty(my_list): 
-        return my_list
+        raise Exception('IndexError: list index out of range')
 
     else:
+        first_element = first_element(my_list)
+        my_list["elements"] = my_list["elements"][1:]
         my_list["size"] = my_list["size"] - 1
-        return first_element(my_list), my_list
+        return first_element
 
 
 
-def remove_last (my_list:array_list):
-
+def remove_last (my_list):
 
     if is_empty(my_list): 
-        return my_list
+        raise Exception('IndexError: list index out of range')
 
     else: 
+        last_element = last_element(my_list)
+        my_list["elements"] = my_list["elements"][:-1]
         my_list["size"] = my_list["size"] - 1
-        return last_element(my_list), my_list
+        return last_element
 
 
 
-def insert_element (my_list:array_list, element, index:int) -> array_list:
+def insert_element (my_list, element, index):
 
     if index > my_list["size"] or index < 0:  
-        return my_list
+        raise Exception('IndexError: list index out of range')
 
     else:  
         my_list["elements"] = my_list["elements"][:index] + [element] + my_list["elements"][index:]
-        my_list["size"] = my_list["size"] + 1
-    
+        my_list["size"] = my_list["size"] + 1 
         return my_list
 
 
 
-def change_info (my_list:array_list, index:int, new_info) -> array_list: 
+def change_info (my_list, index, new_info): 
 
     if index > my_list["size"] or index < 0:  
-        return my_list
+        raise Exception('IndexError: list index out of range')
 
     else: 
-         my_list["elements"] = my_list["elements"][:index] + [new_info] + my_list["elements"][index+1:] 
-         my_list["size"] = my_list["size"]
+         my_list["elements"][index] = new_info
          return my_list
 
 
 
-def exchange (my_list:array_list, index1:int, index2:int) -> array_list:
+def exchange (my_list, index1:int, index2:int):
 
     if (index1 >= my_list["size"] or index1 < 0) or (index2 >= my_list["size"] or index2 < 0):
-        return my_list
+        raise Exception('IndexError: list index out of range')
 
     elif index1 == index2: 
         return my_list
@@ -152,15 +156,14 @@ def exchange (my_list:array_list, index1:int, index2:int) -> array_list:
 
 
 
-def sub_list (my_list:array_list, index:int, num_elements:int) -> array_list:   
+def sub_list (my_list, index:int, num_elements:int):   
 
     if index >= my_list["size"] or index < 0:  
-        return my_list
+        raise Exception('IndexError: list index out of range')
 
     else: 
         
-        sub_list = {"elements": [], "size": 0}
-
+        sub_list = new_list()
         sub_list["elements"] = my_list["elements"][index:index+num_elements]
         sub_list["size"] = len(sub_list["elements"])
         return sub_list
